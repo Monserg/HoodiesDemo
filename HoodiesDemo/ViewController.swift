78c4b5075944e30d78d362a9002ad3c4f3062cef
Sergey Monasturskiy <msm72@mail.ru>
2020-05-14 07:34:57 +0300

    Initial Commit

---
+++ HoodiesDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HoodiesDemo
+//
+//  Created by Sergey Monastyrskiy on 14.05.2020.
+//  Copyright © 2020 Sergey Monastyrskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
